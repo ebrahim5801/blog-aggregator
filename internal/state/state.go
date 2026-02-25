e7328581cd1b29cae9f53bb4ed6679bb7eebefc5
Ebrahim <ebrahimhanna@wecansync.com>
2026-02-25 12:39:42 +0200

    Chapter 2: Database

--- internal/state/state.go
+++ internal/state/state.go
@@ -1,7 +1,11 @@
 package state
 
-import "github.com/ebrahim5801/blog-aggregator/internal/config"
+import (
+	"github.com/ebrahim5801/blog-aggregator/internal/config"
+	"github.com/ebrahim5801/blog-aggregator/internal/database"
+)
 
 type State struct {
+	Db     *database.Queries
 	Config *config.Config
 }
--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/ebrahim5801/blog-aggregator/internal/command"
 	"github.com/ebrahim5801/blog-aggregator/internal/config"
+	"github.com/ebrahim5801/blog-aggregator/internal/database"
 	"github.com/ebrahim5801/blog-aggregator/internal/state"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -18,6 +24,14 @@ func main() {
 
 	s := &state.State{Config: data}
 
+	db, err := sql.Open("postgres", s.Config.DBURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+	dbQueries := database.New(db)
+
+	s.Db = dbQueries
+
 	if len(os.Args) < 2 {
 		fmt.Println("not enough arguments, a command is required")
 		os.Exit(1)
@@ -25,6 +39,9 @@ func main() {
 
 	cmds := &command.Commands{}
 	cmds.Register("login", handlerLogin)
+	cmds.Register("register", handlerRegister)
+	cmds.Register("reset", handlerReset)
+	cmds.Register("users", handlerUsers)
 
 	cmd := command.Command{
 		Name: os.Args[1],
@@ -41,12 +58,73 @@ func handlerLogin(s *state.State, cmd command.Command) error {
 	if len(cmd.Args) == 0 {
 		return fmt.Errorf("please enter username")
 	}
-
-	_, err := s.Config.SetUser(cmd.Args[0])
+	user, err := s.Db.GetUser(context.Background(), cmd.Args[0])
+	if err != nil {
+		os.Exit(1)
+		return err
+	}
+	_, err = s.Config.SetUser(cmd.Args[0])
 	if err != nil {
 		return err
 	}
+	fmt.Println(user)
+	return nil
+}
 
-	fmt.Println("User has been set")
+func handlerRegister(s *state.State, cmd command.Command) error {
+	if len(cmd.Args) == 0 {
+		return fmt.Errorf("please enter username")
+	}
+
+	_, err := s.Db.GetUser(context.Background(), cmd.Args[0])
+	if err != nil {
+		user := database.CreateUserParams{
+			ID:        uuid.New(),
+			Name:      cmd.Args[0],
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		}
+
+		newUser, err := s.Db.CreateUser(context.Background(), user)
+		if err != nil {
+			return err
+		}
+		_, err = s.Config.SetUser(cmd.Args[0])
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(newUser)
+	} else {
+		fmt.Println("user alread exists")
+		os.Exit(1)
+	}
+
+	return nil
+}
+
+func handlerReset(s *state.State, cmd command.Command) error {
+	err := s.Db.DeleteUsers(context.Background())
+	if err != nil {
+		return err
+	}
+	fmt.Println("Reset complete")
+
+	return nil
+}
+
+func handlerUsers(s *state.State, cmd command.Command) error {
+	users, err := s.Db.GetUsers(context.Background())
+	if err != nil {
+		return err
+	}
+	for _, user := range users {
+		fmt.Printf("* %s", user.Name)
+		if user.Name == s.Config.CurrentUserName {
+			fmt.Print(" (current)")
+		}
+		fmt.Print("\n")
+	}
+
 	return nil
 }
